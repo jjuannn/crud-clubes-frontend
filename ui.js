3c79b1ba0d76d74bfba8e9b2648ec65ab6318e56
juan <juancraft12yt@gmail.com>
2020-09-23 23:09:15 -0300

    agrega funciones para ver equipo
    agrega funciones para ver la informacion detallada del equipo. falta agregar la funcionalidad de editar, agregar y eliminar equipo

--- ui.js
+++ ui.js
@@ -1,14 +1,15 @@
-import { obtenerInfoEquipo } from "./api.js"
+import { obtenerInfoEquipo } from "./manager.js"
 
 export function crearCartasEquipos(equipos){
     const container = document.querySelector("#container")
     equipos.forEach( equipo => {
-        
+        const { nombre, fotoEscudo, numeroId } = equipo
+
         const $carta = document.createElement("div")
         $carta.className = "card carta"
 
         const $fotoEscudo = document.createElement("img")
-        $fotoEscudo.src = equipo.fotoEscudo
+        $fotoEscudo.src = fotoEscudo
         $fotoEscudo.className = "card-img-top escudoCarta"
         $carta.append($fotoEscudo)
         
@@ -17,25 +18,44 @@ export function crearCartasEquipos(equipos){
         $cartaBody.className = "card-body"
 
         const nombreEquipo = document.createElement("h5")
-        nombreEquipo.textContent = equipo.nombre
+        nombreEquipo.textContent = nombre
         nombreEquipo.className = "card-title"
         $cartaBody.append(nombreEquipo)
 
         const $verEquipo = document.createElement("a")
-        $verEquipo.className = "btn btn-primary"
+
+        $verEquipo.className = "btn btn-primary button"
         $verEquipo.textContent = "Ver"
+        $verEquipo.setAttribute("data-target", "#label-informacion")
+        $verEquipo.setAttribute("data-toggle", "modal")
+        $verEquipo.addEventListener( "click", async () => {
+            mostrarInformacionEquipo(await obtenerInfoEquipo(numeroId))
+        })
         $cartaBody.append($verEquipo)
 
         const $editarEquipo = document.createElement("a")
-        $editarEquipo.className = "btn btn-warning"
+        $editarEquipo.className = "btn btn-warning button"
         $editarEquipo.textContent = "Editar"
         $cartaBody.append($editarEquipo)
 
         const $borrarEquipo = document.createElement("a")
-        $borrarEquipo.className = "btn btn-danger"
+        $borrarEquipo.className = "btn btn-danger button"
         $borrarEquipo.textContent = "Borrar"
         $cartaBody.append($borrarEquipo)
 
         container.append($carta)
     })
 }
+function mostrarInformacionEquipo(equipo){
+    document.querySelector("#nombre").textContent = equipo.nombre
+    document.querySelector("#escudo").src = equipo.fotoEscudo
+    document.querySelector("#tla").textContent = equipo.abreviatura
+    document.querySelector("#fundacion").textContent = equipo.anoFundacion
+    document.querySelector("#direccion").textContent = equipo.direccion
+    document.querySelector("#estadio").textContent = equipo.estadio
+    document.querySelector("#id").textContent = equipo.numeroId
+    document.querySelector("#pais").textContent = equipo.pais
+    document.querySelector("#telefono").textContent = equipo.telefono
+    document.querySelector("#website").textContent = equipo.website
+
+}   
