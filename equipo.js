81ab887eb76842094149015a371c3dc630d4c6a2
juan <juancraft12yt@gmail.com>
2020-09-23 23:05:09 -0300

    agrega clase equipo
    agrega clase equipo para mapear la respuesta al momento de solicitar la informacion de un equipo

---
+++ equipo.js
@@ -0,0 +1,14 @@
+export class Equipo{
+    constructor(abreviatura, anoFundacion, direccion, estadio, fotoEscudo, nombre, numeroId, pais, telefono, website){
+        this.abreviatura = abreviatura
+        this.anoFundacion = anoFundacion
+        this.direccion = direccion
+        this.estadio = estadio
+        this.fotoEscudo = fotoEscudo
+        this.nombre = nombre
+        this.numeroId = numeroId
+        this.pais = pais
+        this.telefono = telefono
+        this.website = website 
+    }
+}
