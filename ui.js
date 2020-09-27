f0a4effbab41ad1136f366dffd0a2cd9a1df5eb1
juan <juancraft12yt@gmail.com>
2020-09-26 23:00:06 -0300

    agrega funcionalidad para modificar un equipo
    agrega funcionalidad para poder modificar un equipo ya existente. Se mostrara un formulario ya completado con los datos del equipo, donde se podran modificar sus valores y guardarlos

--- ui.js
+++ ui.js
@@ -1,6 +1,5 @@
 import { obtenerInfoEquipo } from "./manager.js"
-import { borrarEquipo } from "./api.js"
-
+import { borrarEquipo, enviarDatosForm } from "./api.js"
 
 export function crearCartasEquipos(equipos){
     const container = document.querySelector("#container")
@@ -28,17 +27,23 @@ export function crearCartasEquipos(equipos){
 
         $verEquipo.className = "btn btn-primary button"
         $verEquipo.textContent = "Ver"
-        $verEquipo.setAttribute("data-target", "#label-informacion")
+        $verEquipo.setAttribute("data-target", "#modal-informacion")
         $verEquipo.setAttribute("data-toggle", "modal")
         $verEquipo.addEventListener( "click", async () => {
-            mostrarInformacionEquipo(await obtenerInfoEquipo(numeroId))
+            mostrarInformacionEquipo(await obtenerInfoEquipo(numeroId, "ver"))
         })
         $cartaBody.append($verEquipo)
 
         const $editarEquipo = document.createElement("a")
         $editarEquipo.className = "btn btn-warning button"
         $editarEquipo.textContent = "Editar"
+        $editarEquipo.setAttribute("data-target", "#modal-editar-equipo")
+        $editarEquipo.setAttribute("data-toggle", "modal")
+        $editarEquipo.addEventListener( "click", async (e) => {
+            mostrarEditarEquipo(await obtenerInfoEquipo(numeroId, "editar"))
+        })
         $cartaBody.append($editarEquipo)
+
 
         const $borrarEquipo = document.createElement("a")
         $borrarEquipo.className = "btn btn-danger button"
@@ -51,21 +56,65 @@ export function crearCartasEquipos(equipos){
         container.append($carta)
     })
 }
-function mostrarInformacionEquipo(equipo){
-    document.querySelector("#nombre").textContent = equipo.nombre
-    document.querySelector("#escudo").src = equipo.fotoEscudo
-    document.querySelector("#tla").textContent = equipo.abreviatura
-    document.querySelector("#fundacion").textContent = equipo.anoFundacion
-    document.querySelector("#direccion").textContent = equipo.direccion
-    document.querySelector("#estadio").textContent = equipo.estadio
-    document.querySelector("#id").textContent = equipo.numeroId
-    document.querySelector("#pais").textContent = equipo.pais
-    document.querySelector("#telefono").textContent = equipo.telefono
-    document.querySelector("#website").textContent = equipo.website
+
+const $form = document.querySelector("#form-editar-equipo")
+
+$form.addEventListener("submit", (e) => {
+
+    const id = document.querySelector("#numeroId-editar").value
+
+    var formElement = document.getElementById("form-editar-equipo")
+    var fileInput = document.querySelector("#imagen-editar");
+    var formData = new FormData(formElement)
+    
+    var file = fileInput.files[0];
+    const foto = URL.createObjectURL(file)
+    formData.append('escudo', foto);
+
+    enviarDatosForm(id ,formData)
+})
+
+function mostrarEditarEquipo(equipo){
+    document.querySelector("#nombre-editar").value = equipo.nombre
+    document.querySelector("#tla-editar").value = equipo.abreviatura
+    document.querySelector("#anoFundacion-editar").value = equipo.anoFundacion
+    document.querySelector("#direccion-editar").value = equipo.direccion
+    document.querySelector("#estadio-editar").value = equipo.estadio
+    document.querySelector("#numeroId-editar").value = equipo.numeroId
+    document.querySelector("#pais-editar").value = equipo.pais
+    document.querySelector("#telefono-editar").value = equipo.telefono
+    document.querySelector("#website-editar").value = equipo.website
 }   
 
+function obtenerValoresDeForm(){
+    const nombre = document.querySelector("[name='nombre']").value
+    const abreviatura = document.querySelector("[name='abreviatura']").value
+    const estadio = document.querySelector("[name='estadio']").value
+    const direccion = document.querySelector("[name='direccion']").value
+    const anoFundacion = document.querySelector("[name='anoFundacion']").value
+    const numeroId = document.querySelector("[name='numeroId']").value
+    const telefono = document.querySelector("[name='telefono']").value
+    const website = document.querySelector("[name='website']").value
+    const pais = document.querySelector("[name='pais']").value
+    const escudo = document.querySelector("[name='escudo']").value
+    
+    return {
+        nombre, abreviatura, estadio, direccion, anoFundacion, numeroId, telefono, website, pais, escudo
+    }
+}
 
-// document.location.reload(true)
+function mostrarInformacionEquipo(equipo){
+    document.querySelector("#nombre-ver").textContent = equipo.nombre
+    document.querySelector("#escudo-ver").src = equipo.fotoEscudo
+    document.querySelector("#tla-ver").textContent = equipo.abreviatura
+    document.querySelector("#fundacion-ver").textContent = equipo.anoFundacion
+    document.querySelector("#direccion-ver").textContent = equipo.direccion
+    document.querySelector("#estadio-ver").textContent = equipo.estadio
+    document.querySelector("#id-ver").textContent = equipo.numeroId
+    document.querySelector("#pais-ver").textContent = equipo.pais
+    document.querySelector("#telefono-ver").textContent = equipo.telefono
+    document.querySelector("#website-ver").textContent = equipo.website
+}   
 
 function borrarEquipoDeLista(id){
     borrarEquipo(id)
